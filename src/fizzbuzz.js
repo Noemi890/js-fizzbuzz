53d473c787e1b5b08a49c07b9b609213bd9dda98
Noemi890 <94084046+Noemi890@users.noreply.github.com>
2022-05-19 15:14:10 +0100

    completed

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,32 @@
 // TODO: Write your function in this file
 
+function fizzBuzz(lower, upper) {
+
+    const fizzBuzzArray = [];
+
+    for (let i = lower; i <= upper; i++) {
+
+    if (i % 3 === 0 && i % 5 !== 0) {
+        fizzBuzzArray.push("Fizz")
+    }
+
+    else if (i % 5 === 0 && i % 3 !== 0) {
+        fizzBuzzArray.push("Buzz")
+    }
+
+    else if (i % 3 === 0 && i % 5 === 0) {
+        fizzBuzzArray.push("FizzBuzz")
+    }
+
+    else {
+        fizzBuzzArray.push(i)
+    }
+
+}
+
+    return fizzBuzzArray;
+
+}
+
 // TODO: Change undefined below to the name of your function
-module.exports = undefined
+module.exports = fizzBuzz
